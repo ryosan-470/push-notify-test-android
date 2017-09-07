c3ad1531fc053834dda075db4d56ece684c66213
Ryosuke Sato <rskjtwp@gmail.com>
2017-09-07 17:25:18 +0900

    ひとまず、ボタンを押すとDevice Tokenを出力するクソアプリ

--- app/src/main/java/ryosan470/push_notify_test/MainActivity.java
+++ app/src/main/java/ryosan470/push_notify_test/MainActivity.java
@@ -2,12 +2,33 @@ package ryosan470.push_notify_test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
         setContentView(R.layout.activity_main);
+
+        Button getDeviceTokenButton = (Button) findViewById(R.id.get_device_token_button);
+        getDeviceTokenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String token = FirebaseInstanceId.getInstance().getToken();
+
+                TextView textView = (TextView) findViewById(R.id.device_token_view);
+                textView.setText(token);
+            }
+        });
     }
 }
