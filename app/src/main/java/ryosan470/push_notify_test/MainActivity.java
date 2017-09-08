e95c8a5a1b1f2b6412fffd77565831bedf6982a2
Ryosuke Sato <rskjtwp@gmail.com>
2017-09-08 16:15:45 +0900

    Add subscribe / unsubscribe button and functions

--- app/src/main/java/ryosan470/push_notify_test/MainActivity.java
+++ app/src/main/java/ryosan470/push_notify_test/MainActivity.java
@@ -6,8 +6,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = "MainActivity";
@@ -29,6 +31,30 @@ public class MainActivity extends AppCompatActivity {
                 TextView textView = (TextView) findViewById(R.id.device_token_view);
                 Log.d(TAG, "Device Token: " + token);
                 textView.setText(token);
+            }
+        });
+
+        Button subscribeTopicButton = (Button) findViewById(R.id.subscribe_topic_button);
+        subscribeTopicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseMessaging.getInstance().subscribeToTopic("news");
+
+                String msg = getString(R.string.msg_subscribed);
+                Log.d(TAG, msg);
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button unsubscribeTopicButton = (Button) findViewById(R.id.unsubscribe_topic_button);
+        unsubscribeTopicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseMessaging.getInstance().unsubscribeFromTopic("news");
+
+                String msg = getString(R.string.msg_unsubscribed);
+                Log.d(TAG, msg);
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
             }
         });
     }
