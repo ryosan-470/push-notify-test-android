07e9c94ff4f3e1bea438da529a5334e81f50fedc
Ryosuke Sato <rskjtwp@gmail.com>
2017-10-10 16:33:11 +0900

    Add subscribe method

--- app/src/main/java/ryosan470/push_notify_test/MyFirebaseInstanceIDService.java
+++ app/src/main/java/ryosan470/push_notify_test/MyFirebaseInstanceIDService.java
@@ -1,14 +1,19 @@
 package ryosan470.push_notify_test;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClient;
 import com.amazonaws.services.sns.model.CreatePlatformEndpointRequest;
 import com.amazonaws.services.sns.model.CreatePlatformEndpointResult;
 import com.amazonaws.services.sns.model.InvalidParameterException;
 import com.amazonaws.services.sns.model.SetEndpointAttributesRequest;
+import com.amazonaws.services.sns.model.SubscribeRequest;
+import com.amazonaws.services.sns.model.SubscribeResult;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
@@ -25,9 +30,17 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
         Log.d(TAG, "Refreshed token: " + refreshedToken);
 
         sendRegistrationToServer(refreshedToken);
+
+        String endpointArn = getEndpointArn();
+        Log.d(TAG, "endpointArn is " + endpointArn);
+        if (endpointArn != null) {
+            SubscribeResult req = addAndroidGroupTopic(endpointArn);
+            Log.d(TAG, "Successful to subscribe : " + req.getSubscriptionArn());
+        }
     }
 
     private void sendRegistrationToServer(String token) {
+        Log.d(TAG, "Called sendRegistrationToServer(" + token + ")");
         AmazonSNSClient client = new AmazonSNSClient(generateAWSCredentials());
         client.setEndpoint(ENDPOINT);
 
@@ -71,10 +84,31 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
 
     private void storeEndpointArn(String endpointArn) {
         // Save endpointArn to a shared preference to cache device token
+        SharedPreferences data = getSharedPreferences("endPointArn", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = data.edit();
+        editor.putString("endpointArn", endpointArn);
+        editor.apply();
     }
 
     private String getEndpointArn() {
         // Get endpointArn from a shared preference
-        return "null";
+        SharedPreferences data = getSharedPreferences("endpointArn", Context.MODE_PRIVATE);
+        String endpointArn = data.getString("endpointArn", null);
+        if (endpointArn == null) {
+            AmazonSNSClient client = new AmazonSNSClient(generateAWSCredentials());
+            client.setEndpoint(ENDPOINT);
+            String token = FirebaseInstanceId.getInstance().getToken();
+            return createEndpointArn(token, client);
+        }
+        return endpointArn;
+    }
+
+    private SubscribeResult addAndroidGroupTopic(String endPoint) {
+        String topicArn = "arn:aws:sns:ap-northeast-1:610199206347:android"; // A group of devices which OS is Android
+        SubscribeRequest req = new SubscribeRequest().withProtocol("application").withTopicArn(topicArn).withEndpoint(endPoint);
+
+        AmazonSNSClient client = new AmazonSNSClient(generateAWSCredentials());
+        client.setEndpoint(ENDPOINT);
+        return client.subscribe(req);
     }
 }
